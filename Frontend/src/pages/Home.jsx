dcde5dfa998796bd8fde222da6a9748141b216aa
FluGoff <siwakorn.wani@kmutt.ac.th>
2025-05-31 12:34:44 +0700

    Update and Create

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../assets/Navbar.jsx";
 
 const Home = () => {
     return (
-        <div className="bg-gray-100 min-h-screen px-4 md:px-16 py-8">
+        <div className="bg-gray-100 min-h-screen px-4 md:px-16 py-18">
             <div className="bg-white rounded shadow-md p-6 max-w-3xl mx-auto mb-10">
                 <h1 className="text-2xl font-semibold text-blue-700 mb-2">Welcome to Paraverse</h1>
                 <p className="text-gray-600 mb-4">Dive into alternate realities and explore fascinating "what if" scenarios.</p>
