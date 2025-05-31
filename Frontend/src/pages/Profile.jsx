2eb0a2c4e2beb1ad01ecf2bed3d9ee0a66dab763
Tantawan7 <supphanyu491@gmail.com>
2025-05-31 13:02:44 +0700

    Update Profile.jsx

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div className="min-h-screen bg-gray-200 flex items-center justify-center p-4">
-      <div className="bg-white border-2 border-black max-w-md w-full p-8 text-center">
+    <div className="min-h-screen bg-gray-200  flex items-center justify-center p-4">
+      <div className="bg-white border-2 border-black rounded-xl max-w-md w-full p-8 text-center">
         {/* Profile Picture Placeholder */}
         <div className="w-24 h-24 bg-gray-300 rounded-full mx-auto mb-6 flex items-center justify-center">
           <div className="w-12 h-12 bg-gray-400 rounded-full"></div>
@@ -23,7 +23,7 @@ const Profile = () => {
           </div>
         </div>
 
-        <button className="bg-[#5885AF] hover:bg-[#334d66] text-white px-8 py-3 text-lg font-medium transition-colors duration-200">
+        <button className="bg-[#5885AF] rounded-xl hover:bg-[#334d66] text-white px-8 py-3 text-lg font-medium transition-colors duration-200 ">
           Edit Profile
         </button>
       </div>
